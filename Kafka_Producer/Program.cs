cde881dd68b98eca2b560b8f10987bc6762b860a
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-11-21 17:02:27 +0530

    Kafka Producer

---
+++ Kafka_Producer/Program.cs
@@ -0,0 +1,13 @@
+﻿using Confluent.Kafka;
+
+var config =new ProducerConfig
+{
+    BootstrapServers = "localhost:9092", // Replace with your Kafka broker address
+    ClientId = "KafkaExampleProducer",
+};
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+string topic = "my-topic"; // Replace with your topic name
+string message = "Hello, Kafka!";
+var deliveryReport = producer.ProduceAsync(topic, new Message<Null, string> { Value = message }).Result;
+Console.WriteLine($"Produced message to {deliveryReport.Topic} partition {deliveryReport.Partition} @ offset {deliveryReport.Offset}");
+Console.ReadLine();
